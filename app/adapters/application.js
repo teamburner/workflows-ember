0d1d2471476cc221a60f3cb5b11b7dc7185097ef
Christian Naths <christiannaths@gmail.com>
2015-04-26 16:11:56 -0600

    production deploy working

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,10 @@
 import DS from "ember-data";
 
 export default DS.RESTAdapter.reopen({
-  namespace: "api"
+  namespace: "api",
+  suffix: '.json',
+
+  pathForType: function(type) {
+    return this._super(type) + this.get('suffix');
+  }
 });
